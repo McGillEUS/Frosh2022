d57eee51ce60f9b2d1c4b816b53e5f325dd9df86
isahernandezfer <isabela.hernandezfernandez@mail.mcgill.ca>
2025-08-02 21:51:20 -0400

    Update registration Steps to Take with new info

--- src/constants/registration.js
+++ src/constants/registration.js
@@ -1,33 +1,35 @@
 //When clicking registration this is the text
 import { Typography } from "@mui/material";
 export const HOW_TO_REGISTER =
-  "Registration opens early August and will be done on the same website for all of the faculties. It is now open at the link below! If you have any questions consult our FAQ page and then feel free to contact us on any of our social medias linked with icons at the bottom of the page.";
+  "Registration opens July 28th and is done on the MyInvolvement website. " + 
+    "It is now open at the link below! Please follow the instructions below to secure your spot for Frosh. " +
+      "If you have any questions, consult our FAQ page and then feel free to contact us on any platform linked at the bottom of the page.";
 
 export const STEPS_TO_TAKE = [
   <Typography variant="p">
-    <strong>Step 1:</strong> Check your McGill email on July 24th. <br></br>
+    <strong>Step 1:</strong> Go to the MyInvolvement website linked below. <br></br>
     <div style={{ padding: "20px 25px 1em 69px"}}> 
     <Typography variant="p">
-      If you are a new student entering the Faculty of Engineering, you will receive a link to our Engineering Frosh ‘MyInvolvement’ registration page. Log in using your McGill log in.
+      Log in using your McGill credentials. Search for "Engineering Frosh". If you are a new student entering the Faculty of Engineering, you should be able to find the event called <b>Cloudy with a Chance of Frosh</b>.
       </Typography>
     </div>
-    <strong>Step 2:</strong> Register following the instructions on MyInvolvement <br></br><br></br>
-    <strong>Step 3:</strong> Check your email for a payment link <br></br>  
+    <strong>Step 2:</strong> Register following the instructions on MyInvolvement. <br></br><br></br>
+    <strong>Step 3:</strong> Check your email for a payment link. <br></br>  
     <div style={{ padding: "20px 25px 1em 69px"}}> 
     <Typography variant="p">
       Each Monday and Thursday at 5PM, people who have registered before that point will receive a payment link through the platform Zeffy. 
       </Typography>
     </div> 
-    <strong>Step 4:</strong> Pay on Zeffy to guarantee your spot. Failure to pay by August 18th may result in your spot given to someone else. <br></br>  
+    <strong>Step 4:</strong> Pay on Zeffy to guarantee your spot. Failure to pay by August 23rd may result in your spot being given to someone else. <br></br>  
     <div style={{ padding: "20px 25px 1em 69px"}}> 
     <Typography variant="p">
-    When prompted to add an additional “Zeffy fee”, select 0% or no. Your spot is <b>not reserved</b> until you have paid as well.
+    Please note that you have to 1) register on MyInvolvement and 2) pay via Zeffy to fully register for Frosh. Your spot is <b>not reserved</b> until you have completed the payment.
       </Typography>
     </div> 
     <strong>Step 5:</strong> Complete the MANDATORY trainings. There are <strong>3</strong> in total: the Sexual Violence
     training called <q>It Takes All of Us</q>, the <q>Anti - Oppression</q>{" "}
-    training and the <q>Frosh 101</q> training all on MyCourses<br></br><br></br>
-    <strong>Step 6:</strong> Come pick up your credentials and gear at your faculties Registration tent (DON'T FORGET YOUR MCGILL ID!),<br></br>  
+    training and the <q>Frosh 101</q> training all on MyCourses. More information on trainings will appear on our Instagram and Facebook in the first couple weeks of August. <br></br><br></br>
+    <strong>Step 6:</strong> Come pick up your credentials and gear at Registration tent (DO NOT FORGET YOUR MCGILL ID!). Registration tent is open from August 23rd - August 27th. <br></br>  
   </Typography>,
   
 ];
